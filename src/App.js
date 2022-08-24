c110555bfca457dcb07b5fbd493b68d7caea33ba
Fienne <ngumihfien@gmail.com>
2022-08-24 12:15:03 +0100

    create app skeleton and link to css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Layout from './Components/layout/layout.component';
 
 function App() {
   let name = "E-Market"
   return (
     <div className="App">
-      
+      <Layout />
     </div>
   );
 }
---
+++ src/Components/layout/layout.component.jsx
@@ -0,0 +1,16 @@
+import "./layout.css"
+
+    const Layout = () => {
+        return <div className="layout">
+            <div className="col-12 navbar-skeleton">
+                {/*My navbar goes here */}
+            </div>
+            <div className="col-2 sidebar-skeleton">
+                {/*My sidebar goes here */}
+            </div>
+            <div className="col-10 main-skeleton">
+                {/*My main body goes here */}
+            </div>
+        </div>
+    }
+    export default Layout
